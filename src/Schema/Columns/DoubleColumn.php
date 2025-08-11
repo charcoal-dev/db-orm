58e234ddd51c21253f7b608ea0cb82a59ac6b796
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 04:39:43 +0500

    Normalize namespace casing, use `PrimitiveType` and `DbDriver` enums, and simplify docblocks in DoubleColumn and FloatColumn classes

--- src/Schema/Columns/DoubleColumn.php
+++ src/Schema/Columns/DoubleColumn.php
@@ -1,28 +1,19 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Columns;
+namespace Charcoal\Database\Orm\Schema\Columns;
 
 /**
  * Class DoubleColumn
- * @package Charcoal\Database\ORM\Schema\Columns
+ * @package Charcoal\Database\Orm\Schema\Columns
  */
 class DoubleColumn extends FloatColumn
 {
-    /**
-     * @param string $name
-     */
     public function __construct(string $name)
     {
         parent::__construct($name);
--- src/Schema/Columns/FloatColumn.php
+++ src/Schema/Columns/FloatColumn.php
@@ -1,49 +1,36 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Columns;
+namespace Charcoal\Database\Orm\Schema\Columns;
 
-use Charcoal\Database\DbDriver;
-use Charcoal\Database\ORM\Schema\Traits\NumericValueTrait;
-use Charcoal\Database\ORM\Schema\Traits\PrecisionValueTrait;
+use Charcoal\Base\Enums\PrimitiveType;
+use Charcoal\Database\Enums\DbDriver;
+use Charcoal\Database\Orm\Schema\Traits\NumericValueTrait;
+use Charcoal\Database\Orm\Schema\Traits\PrecisionValueTrait;
 
 /**
  * Class FloatColumn
- * @package Charcoal\Database\ORM\Schema\Columns
+ * @package Charcoal\Database\Orm\Schema\Columns
  */
 class FloatColumn extends AbstractColumn
 {
-    /** @var string */
-    public const PRIMITIVE_TYPE = "double";
-    /** @var int */
-    protected const MAX_DIGITS = 65;
-    /** @var int */
-    protected const MAX_SCALE = 30;
+    public const PrimitiveType PRIMITIVE_TYPE = PrimitiveType::FLOAT;
 
-    /** @var string */
+    protected const int MAX_DIGITS = 65;
+    protected const int MAX_SCALE = 30;
+
     protected string $type;
-    /** @var int */
     private int $digits = 10;
-    /** @var int */
     private int $scale = 0;
 
     use NumericValueTrait;
     use PrecisionValueTrait;
 
-    /**
-     * @param string $name
-     */
     public function __construct(string $name)
     {
         parent::__construct($name);
@@ -51,9 +38,6 @@ class FloatColumn extends AbstractColumn
         $this->setDefaultValue("0");
     }
 
-    /**
-     * @return array
-     */
     public function __serialize(): array
     {
         $data = parent::__serialize();
@@ -63,10 +47,6 @@ class FloatColumn extends AbstractColumn
         return $data;
     }
 
-    /**
-     * @param array $data
-     * @return void
-     */
     public function __unserialize(array $data): void
     {
         $this->type = $data["type"];
@@ -76,25 +56,17 @@ class FloatColumn extends AbstractColumn
         parent::__unserialize($data);
     }
 
-    /**
-     * @param float|int $value
-     * @return $this
-     */
     public function default(float|int $value = 0): static
     {
         $this->setDefaultValue($value);
         return $this;
     }
 
-    /**
-     * @param \Charcoal\Database\DbDriver $driver
-     * @return string|null
-     */
     public function getColumnSQL(DbDriver $driver): ?string
     {
-        return match ($driver->value) {
-            "mysql" => sprintf('%s(%d,%d)', $this->type, $this->digits, $this->scale),
-            "sqlite" => "REAL",
+        return match ($driver) {
+            DbDriver::MYSQL => sprintf("%s(%d,%d)", $this->type, $this->digits, $this->scale),
+            DbDriver::PGSQL => "REAL",
             default => null,
         };
     }
