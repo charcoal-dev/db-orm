01cde6298bfe8322774042251bab37f5c15d5889
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 04:31:39 +0500

    Normalize namespace casing, simplify docblocks, and refactor `Constraints` class to use `InstancedObjectsRegistry` and `RegistryKeysLowercaseTrimmed` traits.

--- src/Schema/Constraints.php
+++ src/Schema/Constraints.php
@@ -1,80 +1,59 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema;
+namespace Charcoal\Database\Orm\Schema;
 
-use Charcoal\Database\ORM\Schema\Constraints\ForeignKeyConstraint;
-use Charcoal\Database\ORM\Schema\Constraints\IndexKeyConstraint;
-use Charcoal\Database\ORM\Schema\Constraints\UniqueKeyConstraint;
+use Charcoal\Base\Concerns\InstancedObjectsRegistry;
+use Charcoal\Base\Concerns\RegistryKeysLowercaseTrimmed;
+use Charcoal\Database\Orm\Schema\Constraints\AbstractConstraint;
+use Charcoal\Database\Orm\Schema\Constraints\ForeignKeyConstraint;
+use Charcoal\Database\Orm\Schema\Constraints\IndexKeyConstraint;
+use Charcoal\Database\Orm\Schema\Constraints\UniqueKeyConstraint;
 
 /**
  * Class Constraints
- * @package Charcoal\Database\ORM\Schema
+ * @package Charcoal\Database\Orm\Schema
+ * @property array<string,AbstractConstraint> $instances
  */
 class Constraints implements \IteratorAggregate
 {
-    private array $constraints = [];
+    use InstancedObjectsRegistry;
+    use RegistryKeysLowercaseTrimmed;
 
-    /**
-     * @param string $key
-     * @return \Charcoal\Database\ORM\Schema\Constraints\UniqueKeyConstraint
-     */
     public function uniqueKey(string $key): UniqueKeyConstraint
     {
         $constraint = new UniqueKeyConstraint($key);
-        $this->constraints[$key] = $constraint;
+        $this->instances[$key] = $constraint;
         return $constraint;
     }
 
-    /**
-     * @param string $key
-     * @return ForeignKeyConstraint
-     */
     public function foreignKey(string $key): ForeignKeyConstraint
     {
         $constraint = new ForeignKeyConstraint($key);
-        $this->constraints[$key] = $constraint;
+        $this->instances[$key] = $constraint;
         return $constraint;
     }
 
-    /**
-     * @param string $column
-     * @param string $prefix
-     * @return void
-     */
     public function addIndex(string $column, string $prefix = "idx_"): void
     {
         $key = $prefix . $column;
-        $this->constraints[$key] = (new IndexKeyConstraint($key))->columns($column);
+        $this->instances[$key] = (new IndexKeyConstraint($key))->columns($column);
     }
 
-    /**
-     * @param string $key
-     * @return IndexKeyConstraint
-     */
     public function addIndexComposite(string $key): IndexKeyConstraint
     {
         $constraint = new IndexKeyConstraint($key);
-        $this->constraints[$key] = $constraint;
+        $this->instances[$key] = $constraint;
         return $constraint;
     }
 
-    /**
-     * @return \Traversable
-     */
     public function getIterator(): \Traversable
     {
-        return new \ArrayIterator($this->constraints);
+        return new \ArrayIterator($this->instances);
     }
 }
