236683fa45b6b813b6e502bd2ca4629732ea83e4
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 04:21:42 +0500

    Normalize namespace casing, simplify docblocks, and refactor `BoolColumn` and `BufferColumn` to use updated resolver methods in attributes.

--- src/Schema/Columns/BoolColumn.php
+++ src/Schema/Columns/BoolColumn.php
@@ -1,62 +1,46 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Columns;
+namespace Charcoal\Database\Orm\Schema\Columns;
 
+use Charcoal\Base\Enums\PrimitiveType;
 use Charcoal\Database\DbDriver;
 
 /**
  * Class BoolColumn
- * @package Charcoal\Database\ORM\Schema\Columns
+ * @package Charcoal\Database\Orm\Schema\Columns
  */
 class BoolColumn extends AbstractColumn
 {
-    public const PRIMITIVE_TYPE = "integer";
+    public const PrimitiveType PRIMITIVE_TYPE = PrimitiveType::INT;
 
-    /**
-     * @return void
-     */
     protected function attributesCallback(): void
     {
         $this->attributes->unSigned = true;
-        $this->attributes->setModelsValueResolver(function (mixed $value): bool {
+        $this->attributes->resolveTypedValue(function (mixed $value): bool {
             return $value === 1;
         });
 
-        $this->attributes->setModelsValueDissolveFn(function (mixed $value): int {
+        $this->attributes->resolveDbValue(function (mixed $value): int {
             return is_bool($value) && $value ? 1 : 0;
         });
     }
 
-    /**
-     * @param bool $defaultValue
-     * @return $this
-     */
     public function default(bool $defaultValue): static
     {
         $this->setDefaultValue($defaultValue ? 1 : 0);
         return $this;
     }
 
-    /**
-     * @param \Charcoal\Database\DbDriver $driver
-     * @return string|null
-     */
     public function getColumnSQL(DbDriver $driver): ?string
     {
-        return match ($driver->value) {
-            "mysql" => "tinyint",
+        return match ($driver) {
+            DbDriver::MYSQL => "tinyint",
             default => "integer",
         };
     }
--- src/Schema/Columns/BufferColumn.php
+++ src/Schema/Columns/BufferColumn.php
@@ -1,35 +1,26 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Columns;
+namespace Charcoal\Database\Orm\Schema\Columns;
 
 use Charcoal\Buffers\AbstractByteArray;
 use Charcoal\Buffers\Buffer;
 
 /**
  * Class BufferColumn
- * @package Charcoal\Database\ORM\Schema\Columns
+ * @package Charcoal\Database\Orm\Schema\Columns
  */
 class BufferColumn extends BlobColumn
 {
-    /**
-     * @return void
-     */
     protected function attributesCallback(): void
     {
-        $this->attributes->setModelsValueResolver(Buffer::class);
-        $this->attributes->setModelsValueDissolveFn(function (?AbstractByteArray $byteArray): ?string {
+        $this->attributes->resolveTypedValue(Buffer::class);
+        $this->attributes->resolveDbValue(function (?AbstractByteArray $byteArray): ?string {
             return $byteArray?->raw();
         });
     }
