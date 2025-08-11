0cceeec6ce468a2d9af78da74d99d600dffe8a5f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 01:01:16 +0500

    Normalize namespace casing and update docblocks in schema traits

--- src/Schema/Traits/ColumnCharsetTrait.php
+++ src/Schema/Traits/ColumnCharsetTrait.php
@@ -1,30 +1,24 @@
 <?php
 /*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Traits;
+namespace Charcoal\Database\Orm\Schema\Traits;
 
-use Charcoal\Database\ORM\Schema\Charset;
+use Charcoal\Database\Orm\Schema\Charset;
 
 /**
  * Trait ColumnCharsetTrait
- * @package Charcoal\Database\ORM\Schema\Traits
+ * @package Charcoal\Database\Orm\Schema\Traits
  * @internal 
  */
 trait ColumnCharsetTrait
 {
     /**
-     * @param \Charcoal\Database\ORM\Schema\Charset $charset
+     * @param \Charcoal\Database\Orm\Schema\Charset $charset
      * @return $this
      */
     final public function charset(Charset $charset): static
--- src/Schema/Traits/LengthValueTrait.php
+++ src/Schema/Traits/LengthValueTrait.php
@@ -1,22 +1,16 @@
 <?php
 /*
- * This file is a part of "charcoal-dev/db-orm" package.
- * https://github.com/charcoal-dev/db-orm
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/db-orm/blob/main/LICENSE
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\ORM\Schema\Traits;
+namespace Charcoal\Database\Orm\Schema\Traits;
 
 /**
  * Trait LengthValueTrait
- * @package Charcoal\Database\ORM\Schema\Traits
+ * @package Charcoal\Database\Orm\Schema\Traits
  * @internal
  */
 trait LengthValueTrait
