af794653c8d43085cf4f95a5015e01663042a16c
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 11:39:02 +0500

    Rename `Constraints` namespace to `Builder\Constraints` and update affected classes.

--- src/Schema/Builder/Constraints/IndexKeyConstraint.php
+++ src/Schema/Builder/Constraints/IndexKeyConstraint.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace Charcoal\Database\Orm\Schema\Constraints;
+namespace Charcoal\Database\Orm\Schema\Builder\Constraints;
 
 use Charcoal\Database\Enums\DbDriver;
 
