3fe92ba9fbfd26265936f4520bcc94d2c700fd89
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 11:06:58 +0500

    Add `BackedEnumColumnPipe` for handling `BackedEnum` database transformations

---
+++ src/Pipes/BackedEnumColumnPipe.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Database\Orm\Pipes;
+
+use Charcoal\Base\Support\Runtime;
+use Charcoal\Database\Orm\Contracts\ColumnValuePipeInterface;
+use Charcoal\Database\Orm\Schema\Snapshot\ColumnSnapshot;
+
+/**
+ * Store/Retrieve \BackedEnum values from the database.
+ */
+final readonly class BackedEnumColumnPipe implements ColumnValuePipeInterface
+{
+    public static function forDb(mixed $value, ColumnSnapshot $context): string
+    {
+        Runtime::assert($value instanceof \BackedEnum,
+            "BackedEnumColumnPipe: value must be a BackedEnum, got " . get_debug_type($value));
+
+        /** @var $value \BackedEnum */
+        return (string)$value->value;
+    }
+
+    public static function forEntity(string|int|array $value, ColumnSnapshot $context): \BackedEnum
+    {
+        Runtime::assert(is_string($value) || is_int($value),
+            "BackedEnumColumnPipe: value must be a string|int, got " . get_debug_type($value));
+
+        /** @var \BackedEnum $enum */
+        $enum = $context->pipeContextFqcn;
+        return $enum::from($value);
+    }
+}
