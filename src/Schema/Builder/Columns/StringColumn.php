8737fb2f77ce6d6f1a34dd5013d64b68c422221c
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 11:15:17 +0500

    Rename `StringColumn` namespace, refactor to extend `AbstractColumnBuilder`, and update constructor to use `ColumnType::String`.

---
+++ src/Schema/Builder/Columns/StringColumn.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Part of the "charcoal-dev/db-orm" package.
+ * @link https://github.com/charcoal-dev/db-orm
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Database\Orm\Schema\Builder\Columns;
+
+use Charcoal\Database\Enums\DbDriver;
+use Charcoal\Database\Orm\Enums\ColumnType;
+use Charcoal\Database\Orm\Schema\Builder\Traits\ColumnCharsetTrait;
+use Charcoal\Database\Orm\Schema\Builder\Traits\LengthValueTrait;
+use Charcoal\Database\Orm\Schema\Builder\Traits\StringValueTrait;
+use Charcoal\Database\Orm\Schema\Builder\Traits\UniqueValueTrait;
+
+/**
+ * Class StringColumn
+ * @package Charcoal\Database\Orm\Schema\Columns
+ */
+class StringColumn extends AbstractColumnBuilder
+{
+    protected const int LENGTH_MIN = 1;
+    protected const int  LENGTH_MAX = 0xffff;
+
+    use ColumnCharsetTrait;
+    use LengthValueTrait;
+    use StringValueTrait;
+    use UniqueValueTrait;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name, ColumnType::String);
+    }
+
+    public function getColumnSQL(DbDriver $driver): ?string
+    {
+        return match ($driver) {
+            DbDriver::MYSQL => sprintf('%s(%d)', ($this->fixed ? "char" : "varchar"), $this->length),
+            default => "TEXT"
+        };
+    }
+}
