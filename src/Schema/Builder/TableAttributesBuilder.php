cbb917616a6a5af9084bd03b5da4b8d80274a26e
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 12:23:14 +0500

    Remove unused `TableAttributes` dependency and arbitrary attributes handling from `TableAttributesBuilder`

--- src/Schema/Builder/TableAttributesBuilder.php
+++ src/Schema/Builder/TableAttributesBuilder.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 namespace Charcoal\Database\Orm\Schema\Builder;
 
 use Charcoal\Database\Orm\Enums\MySqlEngine;
-use Charcoal\Database\Orm\Schema\Snapshot\TableAttributes;
 
 /**
  * A builder class for managing table attributes, allowing the user to
@@ -18,26 +17,4 @@ use Charcoal\Database\Orm\Schema\Snapshot\TableAttributes;
 class TableAttributesBuilder
 {
     public ?MySqlEngine $mysqlEngine;
-    private array $arbitrary = [];
-
-    public function set(string $key, int|string $value): static
-    {
-        $this->arbitrary[$key] = $value;
-        return $this;
-    }
-
-    public function get(string $key): int|string|null
-    {
-        return $this->arbitrary[$key] ?? null;
-    }
-
-    public function has(string $key): bool
-    {
-        return isset($this->arbitrary[$key]);
-    }
-
-    public function snapshot(): TableAttributes
-    {
-        return new TableAttributes($this->mysqlEngine);
-    }
 }
