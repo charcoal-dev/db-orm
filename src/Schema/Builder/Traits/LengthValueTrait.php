bb9ab77229f29538bc02be5d3594832d79b3ff0e
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 03:09:03 +0500

    doc update

--- src/Schema/Builder/Traits/LengthValueTrait.php
+++ src/Schema/Builder/Traits/LengthValueTrait.php
@@ -14,11 +14,16 @@ trait LengthValueTrait
     protected int $length = 255;
     protected bool $fixed = false;
 
+    /**
+     * Arbitrary length values within the specified maximum boundary.
+     */
     public function length(int $length): static
     {
         if ($length < static::LENGTH_MIN || $length > static::LENGTH_MAX) {
             throw new \OutOfRangeException(
-                sprintf('Maximum length for col "%s" cannot exceed %d', $this->attributes->name, static::LENGTH_MAX)
+                sprintf('Maximum length for col "%s" cannot exceed %d',
+                    $this->attributes->name,
+                    static::LENGTH_MAX)
             );
         }
 
@@ -26,6 +31,11 @@ trait LengthValueTrait
         return $this;
     }
 
+    /**
+     * Marks the column as fixed-length.
+     * CHECK constraint will require the column to be exactly the specified length.
+     * Value will not be automatically NULL padded.
+     */
     public function fixed(int $length): static
     {
         $this->length($length);
