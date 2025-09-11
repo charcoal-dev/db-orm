7063b915daadf0af4cdf65c17839bb008cddaa62
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-11 07:04:49 +0500

    Enhanced RegExp handling and SQLite behavior; added GMP adapter and extensions dependencies

--- src/CharcoalOrm.php
+++ src/CharcoalOrm.php
@@ -13,8 +13,19 @@ namespace Charcoal\Database\Orm;
  */
 final class CharcoalOrm
 {
-    /** @var bool Global switch to silently discard CHECK REGEXP for SQLite */
-    public static bool $sqLiteEmitRegExp = false;
+    /**
+     * Global switch to silently discard CHECK REGEXP for SQLite.
+     * SQLite3 support can be enabled using Charcoal\Database\Support\SQLite3::enableRegExpLike() method.
+     * @see \Charcoal\Database\Support\SQLite3::enableRegExpLike()
+     */
+    public static bool $sqLiteEmitRegExp = true;
+
+    /** CHECK RegExp behavior control */
+    public static bool $constraintRegExpEscapeQuotes = true;
+    public static bool $constraintRegExpTrimSlashes = true;
+
+    /** language=RegExp Global RegExp for table, column and constraint names  */
+    public const string NAME_REGEXP = "/^[a-z][a-z0-9_]{1,62}$/";
 
     /** @var string[] Comprehensive index of SQL reserved keywords */
     public const array RESERVED_KEYWORDS = [
@@ -26,11 +37,35 @@ final class CharcoalOrm
         "update", "user", "using", "values", "view", "when", "where", "with", "without"
     ];
 
+    /** @var string[] Comprehensive index of DSV delimiters */
+    public const array DSV_DELIMITERS = [" ", ",", "\t", "|", ";", ":"];
+
     /**
      * Checks if the given argument is a reserved SQL keyword.
      */
     public static function isReserved(string $word): bool
     {
         return in_array(strtolower($word), self::RESERVED_KEYWORDS, true);
+    }
+
+    /**
+     * Returns normalized RegExp to be used in CHECK constraints.
+     */
+    public static function getConstraintRegExp(string $column, string $regExp): array
+    {
+        if (!$regExp || @preg_match($regExp, "") === false) {
+            throw new \InvalidArgumentException("Bad RegExp pattern for column: " . $column);
+        }
+
+        $delimiter = $regExp[0];
+        preg_match("/" . preg_quote($delimiter, "/") . "[a-z]*\z/", $regExp, $flags);
+        $flags = substr($flags[0] ?? "\0", 1);
+        if ($flags && !preg_match("/\A[im]*\z/", $flags)) {
+            throw new \InvalidArgumentException("RegExp flags must be a combination of i or m");
+        }
+
+        if (self::$constraintRegExpTrimSlashes) $regExp = substr($regExp, 1, -1 * (strlen($flags) + 1));
+        if (self::$constraintRegExpEscapeQuotes) $regExp = str_replace("'", "''", $regExp);
+        return [$regExp, $flags ?: null];
     }
 }
